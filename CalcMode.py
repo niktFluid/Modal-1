9cf5403dcbf735dd8a2858a67e168d0176acf531
Yoimi KOJIMA <y-kojima@st.go.tuat.ac.jp>
2020-02-18 17:51:24 -0800

    Made modification for sweeping on the resolvent analysis.

--- CalcMode.py
+++ CalcMode.py
@@ -43,9 +43,17 @@ def main(param_file='Parameter.dat', profile='Default'):
         CalcStability(case_dir, time_dir, operator, save_name, k=k, sigma=sigma, which=which)
 
     elif mode == 'Resolvent':
-        omega = float(params['Omega'])
-        alpha = float(params['Alpha'])
         r_mode = params['ResolventMode']
+
+        s_omega = float(params['OmegaStart'])
+        e_omega = float(params['OmegaEnd'])
+        n_omega = int(params['OmegaNum'])
+        omega = (s_omega, e_omega, n_omega)
+
+        s_alpha = float(params['AlphaStart'])
+        e_alpha = float(params['AlphaEnd'])
+        n_alpha = int(params['AlphaNum'])
+        alpha = (s_alpha, e_alpha, n_alpha)
 
         CalcResolvent(case_dir, time_dir, operator, save_name, k=k, omega=omega, alpha=alpha, mode=r_mode)
 
@@ -76,11 +84,21 @@ def CalcStability(case_dir, time, operator_name, save_name, k=3, sigma=None, whi
     ls_mode.vis_tecplot(save_name + '.dat')
 
 
-def CalcResolvent(case_dir, time, operator_name, save_name, k=3, omega=0.0, alpha=0.0, mode='Both'):
+def CalcResolvent(case_dir, time, operator_name, save_name, k=3, omega=None, alpha=None, mode='Both'):
     mesh = OfMesh(case_dir, time + 'C', time + 'V', time + 'U', time + 'p')
     ave_field = OfData(mesh, case_dir + time, 'UMean', 'pMean', 'rhoMean')
 
-    grid_list = [(omega, alpha)]
+    if isinstance(omega, tuple):
+        omega_array = np.linspace(*omega)
+    else:
+        omega_array = np.array([omega])
+
+    if isinstance(alpha, tuple):
+        alpha_array = np.linspace(*alpha)
+    else:
+        alpha_array = np.array([alpha])
+
+    grid_list = [(o, a) for o, a in product(omega_array, alpha_array)]
     resolvent_mode = Resolvent(mesh, ave_field, operator_name, k=k, omega=omega, alpha=alpha, mode=mode)
     resolvent_mode.solve(grid_list, save_name)
 
